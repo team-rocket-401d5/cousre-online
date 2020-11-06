b0604fac6b9835352737f529dcc8ed64df598d7b
bayanalalem <bayanalalem0@gmail.com>
2020-11-06 19:38:43 +0200

    google aouth done

---
+++ public/google.js
@@ -0,0 +1,22 @@
+// let URL = 'https://accounts.google.com/o/oauth2/auth/oauthchooseaccount';
+let URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+let options = {
+  client_id:
+    '1014187475049-t1ia1v1in4t4us48lmlsugme36of0as0.apps.googleusercontent.com',
+  redirect_uri: 'http://localhost:4000/oauth',
+  response_type: 'code',
+  scope: 'profile email',
+  fetch_basic_profile: true,
+};
+
+let QueryString = Object.keys(options)
+  .map((key) => {
+    return `${key}=` + encodeURIComponent(options[key]);
+  })
+  .join('&');
+
+let authURL = `${URL}?${QueryString}`;
+
+let link = document.getElementById('oauth');
+link.setAttribute('href', authURL);
