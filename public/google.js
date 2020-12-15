57a4f2e1859f0f6cac79c61c91aecd205adb07a9
dinaAlsaid <dinaAlsaid@github.com>
2020-12-15 16:41:43 +0200

    google redirect uri

--- public/google.js
+++ public/google.js
@@ -4,7 +4,7 @@ let URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 let options = {
   client_id:
     '1014187475049-t1ia1v1in4t4us48lmlsugme36of0as0.apps.googleusercontent.com',
-  redirect_uri: 'https://course-fellows.herokuapp.com/oauth',
+  redirect_uri: 'https://course-fellows.netlify.app/oauth',
   response_type: 'code',
   scope: 'profile email',
   fetch_basic_profile: true,
